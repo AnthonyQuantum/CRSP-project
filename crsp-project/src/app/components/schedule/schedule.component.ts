4a5f72a982822df6107805c163b7037ff3140925
AnthonyQuantum <quantumcreeper12@gmail.com>
2018-03-20 20:01:11 +0300

    Added basic rows in schedule

--- crsp-project/src/app/components/schedule/schedule.component.ts
+++ crsp-project/src/app/components/schedule/schedule.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { DataService } from '../../services/data/data.service';
+
+class TimeRow {
+  title: string;
+  task: any;
+
+  constructor(title: string, task: any) {
+    this.title = title;
+    this.task = task;
+  }
+}
 
 @Component({
   selector: 'schedule',
@@ -10,25 +21,44 @@ export class ScheduleComponent implements OnInit {
 
 now: Date = new Date();
 
-timeRows = [];
-iter = 1;
+timeRows: Array<TimeRow>;
+tasks: Array<any>;
 
-  constructor() {
-    while(this.iter < 12)
-    {
-      this.timeRows.push(this.iter + "am");
-      this.iter++;
-    }
-    this.timeRows.push("12pm");
-    this.iter = 1;
-    while(this.iter < 12)
-    {
-      this.timeRows.push(this.iter + "pm");
-      this.iter++;
-    }
+  constructor(private _dataService: DataService) {
+    this._dataService.getTasks()
+      .subscribe(res => { 
+        this.tasks = res; 
+        this.generateTimeRows(); 
+      });
+      
    }
 
   ngOnInit() {
+  }
+
+  generateTimeRows() {
+    let iter = 1;
+    let timeRow: TimeRow; 
+    let title: string;
+    this.timeRows = [];
+    let Task: any;
+
+    while(iter < 24)
+    {
+      if (iter < 12)
+        title = iter + "am";
+      else if (iter == 12)
+      {
+        title = "12pm";
+      }
+      else
+        title = (iter-12) + "pm";
+
+      Task = this.tasks.find(t => t.priority == 'T' && t.startTime == title);
+      timeRow = new TimeRow(title, Task);
+      this.timeRows.push(timeRow);
+      iter++;
+    }
   }
 
 }
