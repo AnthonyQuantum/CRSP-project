0cbcc21b0840a6dd47126a99b4d2788831aa2cdf
AnthonyQuantum <quantumcreeper12@gmail.com>
2018-03-25 21:15:52 +0300

    Rhythm component: added real graph values

--- crsp-project/src/app/components/rhythms/rhythms.component.ts
+++ crsp-project/src/app/components/rhythms/rhythms.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import { TimeService } from '../../services/time/time.service';
 
 @Component({
   selector: 'rhythms',
@@ -10,18 +11,35 @@ export class RhythmsComponent implements AfterViewInit {
 
   canvas: any;
   ctx: any;
+  timesShow = ["12am", "2am", "4am", "6am", "8am", "10am", "12pm", "2pm", "4pm", "6pm", "8pm", "10pm", "12am"];
+  valuesShow = [90,70,53,100,120,132,115,100,100,107,111,103,90];
+
+  times = [];
+  timeTitles = [];
+  values = [90,85,80,75,70,65,60,56,53,70,80,90,100,105,
+    110,115,120,123,129,131,132,131,125,120,115,111,
+    106,102,101,100,100,100,101,103,105,106,107,108,
+    110,111,111,110,108,105,103,100,96,93,90];
 
   // Generate chart
   ngAfterViewInit() {
+    this.times = this._time.generateTimes();
+    for (let time of this.times)
+    {
+      this.timeTitles.push(this.timeToTitle(time));
+    }
+    this.timeTitles.push("12am");
+    console.log(this.timeTitles);
+
     this.canvas = document.getElementById('myChart');
     this.ctx = this.canvas.getContext('2d');
     let myChart = new Chart(this.ctx, {
       type: 'line',
       data: {
-          labels: ["6am", "8am", "10am", "12pm", "2pm", "4pm", "6pm", "8pm", "10pm", "12am", "2am", "4am", "6am"],
+          labels: this.timesShow,
           datasets: [{
               label: 'Natural rhythm',
-              data: [100,120,132,115,100,100,107,111,103,90,70,53,100],
+              data: this.valuesShow,
               backgroundColor: [
                   '#1E90FF',
                   '#1E90FF',
@@ -39,6 +57,11 @@ export class RhythmsComponent implements AfterViewInit {
     });
   }
 
-  constructor() {}
+  constructor(private _time: TimeService) {}
+
+  timeToTitle(time: number)
+  {
+    return this._time.timeToTitle(time);
+  }
 
 }
